5d6bbae0c03971e24edd3188fb70d33f2fe3c540
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-11-22 21:47:40 -0300

    arrays 1

---
+++ typescript-basico/arrays/script.ts
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30, 40, 50];
+const valores = [10, "Produto 2", 20, "1", 30, 40, 50, "Produto 1"];
+
+function maiorQue10(data: number[]) {
+  return data.filter(num => num > 10)
+}
+
+function filtrarValores(data: Array<number | string>) {
+  return data.filter(item => typeof item === 'number')
+}
+
+console.log(maiorQue10(numbers))
+console.log(filtrarValores(valores))
