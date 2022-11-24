58c26f3205a1c710f4423d7b1ad0e2d892451a77
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-11-23 23:10:26 -0300

    any

---
+++ typescript-basico/any/script.js
@@ -0,0 +1,8 @@
+"use strict";
+function normalizar(texto) {
+    return texto.trim().toLowerCase();
+}
+function normalizarString(texto) {
+    return texto.trim().toLowerCase();
+}
+console.log(normalizarString("See W"));
---
+++ typescript-basico/any/script.ts
@@ -0,0 +1,9 @@
+function normalizar(texto: any) {
+  return texto.trim().toLowerCase();
+}
+
+function normalizarString(texto: string) {
+  return texto.trim().toLowerCase();
+}
+
+console.log(normalizarString("See W"))
