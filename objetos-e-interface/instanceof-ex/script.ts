400ded0a41a539227b2bb33fc10e9e9b8c0b0022
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-11-27 23:10:05 -0300

    instaceof

--- objetos-e-interface/instanceof-ex/script.ts
+++ objetos-e-interface/instanceof-ex/script.ts
@@ -3,4 +3,5 @@ const link = document.getElementById('ex');
 if (link instanceof HTMLAnchorElement) {
   // link.setAttribute('href', 'https://www.origamid.com');
   link.href = link.href.replace('http://', 'https://')
+  console.log(link)
 }
