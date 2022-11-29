be23daea63475484073d0018ae0387c6170d1d70
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-11-29 08:34:27 -0300

    interface dom

---
+++ objetos-e-interface/interface-dom/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const video = document.querySelector("#videoprincipal");
+if (video instanceof HTMLVideoElement) {
+    console.log(video.volume);
+}
---
+++ objetos-e-interface/interface-dom/script.ts
@@ -0,0 +1,6 @@
+const video = document.querySelector("#videoprincipal");
+
+if (video instanceof HTMLVideoElement) {
+
+  console.log(video.volume)
+}
