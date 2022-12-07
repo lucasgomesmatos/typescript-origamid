f3004b6b929ac3d6eaa7803e639c9e740da0112c
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-12-06 21:27:55 -0300

    generic

--- objetos-e-interface/generic-1/script.js
+++ objetos-e-interface/generic-1/script.js
@@ -1,7 +1,14 @@
 "use strict";
-function retorno(a) {
-    return a;
+// function retorno<T>(a: T): T {
+//   return a;
+// }
+// console.log(retorno<string>("Novo Game").toUpperCase())
+// console.log(retorno(200.00))
+// console.log(retorno(true))
+const frutas = ['uva', 'pera', 'laranja', 'limão', 'maçã'];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function list(list) {
+    return list.slice(1, 9);
 }
-console.log(retorno("Novo Game").toUpperCase());
-console.log(retorno(200.00));
-console.log(retorno(true));
+console.log(list(frutas));
+console.log(list(numeros).filter(item => item >= 6 ?? item));
--- objetos-e-interface/generic-1/script.ts
+++ objetos-e-interface/generic-1/script.ts
@@ -1,9 +1,19 @@
 
-function retorno<T>(a: T): T {
-  return a;
+// function retorno<T>(a: T): T {
+//   return a;
+// }
+
+// console.log(retorno<string>("Novo Game").toUpperCase())
+// console.log(retorno(200.00))
+// console.log(retorno(true))
+
+
+const frutas = ['uva', 'pera', 'laranja', 'limão', 'maçã'];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function list<T>(list: T[]) {
+  return list.slice(1, 9);
 }
 
-
-console.log(retorno<string>("Novo Game").toUpperCase())
-console.log(retorno(200.00))
-console.log(retorno(true))
+console.log(list(frutas));
+console.log(list(numeros).filter(item => item >= 6 ?? item));
