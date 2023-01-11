8943df7805451138ca1859d75fa2a12c013865c8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-10 22:22:19 -0300

    typeguard

--- objetos-e-interface/type-guard/script.js
+++ objetos-e-interface/type-guard/script.js
@@ -1,71 +1,24 @@
-'use strict';
-function typeGuard(value) {
-  if (typeof value === 'string') {
-    return value.toLowerCase();
-  }
-  if (typeof value === 'number') {
-    return value.toFixed();
-  }
-  if (value instanceof HTMLElement) {
-    return value.innerText;
-  }
-}
-typeGuard('Origamid');
-typeGuard(200);
-typeGuard(document.body);
-const obj = {
-  nome: 'Origamid',
-};
-if ('nome' in obj) {
-  console.log('Sim');
-}
-async function fetchProduto() {
-  const response = await fetch('https://api.origamid.dev/json/notebook.json');
-  const json = await response.json();
-  handleProduto(json);
-}
-function handleProduto(data) {
-  if ('preco' in data) {
-    document.body.innerHTML += `
-      <p>Nome: ${data.nome}</p>
-      <p>Preço: R$ ${data.preco + 100} </p>
-    `;
-  }
-}
-fetchProduto();
-async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
-  const json = await response.json();
-  handleCursos(json);
-}
-fetchCursos();
-function handleCursos(data) {
-  if (data instanceof Array) {
-    console.log('É instância de Array');
-  }
-  if (Array.isArray(data)) {
-    console.log('É array');
-  }
-}
-/**
- * Função type predicate, verifica se o value é uma string e retorna um boolean
- * @param value string
- * @returns boolean
- */
+"use strict";
+// async function fetchCursos() {
+//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
+//   const json = await response.json();
+//   handleCursos(json);
+// }
+// fetchCursos();
+// function handleCursos(data: unknown) {
+//   if (data instanceof Array) {
+//     console.log('É instância de Array');
+//   }
+//   if (Array.isArray(data)) {
+//     console.log('É array');
+//   }
+// }
 function isString(value) {
-  if (typeof value === 'string') {
-    return true;
-  }
-  return false;
+    return typeof value === 'string';
 }
-
-/**
- *
- * @param {*} data
- */
 function handleData(data) {
-  if (isString(data)) {
-    console.log(data.toUpperCase());
-  }
+    if (isString(data)) {
+        return data.toUpperCase();
+    }
 }
-handleData('teste');
+console.log(handleData('Origamid'));
--- objetos-e-interface/type-guard/script.ts
+++ objetos-e-interface/type-guard/script.ts
@@ -1,37 +1,27 @@
-async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
-  const json = await response.json();
-  handleCursos(json);
-}
-fetchCursos();
+// async function fetchCursos() {
+//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
+//   const json = await response.json();
+//   handleCursos(json);
+// }
+// fetchCursos();
 
-function handleCursos(data: unknown) {
-  if (data instanceof Array) {
-    console.log('É instância de Array');
-  }
-  if (Array.isArray(data)) {
-    console.log('É array');
-  }
-}
+// function handleCursos(data: unknown) {
+//   if (data instanceof Array) {
+//     console.log('É instância de Array');
+//   }
+//   if (Array.isArray(data)) {
+//     console.log('É array');
+//   }
+// }
 
-/**
- * Função type predicate, verifica se o value é uma string e retorna um boolean
- * @param value string
- * @returns boolean
- */
 function isString(value: unknown): value is string {
-  if (typeof value === 'string') {
-    return true;
-  }
-
-  return false;
+  return typeof value === 'string';
 }
 
 function handleData(data: unknown) {
   if (isString(data)) {
-    console.log(data.toUpperCase());
-
+    return data.toUpperCase();
   }
 }
 
-handleData("teste")
+console.log(handleData('Origamid'));
