5bbbacce44696ef724607cbeb42556369742642b
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-15 23:06:00 -0300

    type assertion

--- objetos-e-interface/type-guard-2/script.ts
+++ objetos-e-interface/type-guard-2/script.ts
@@ -1,49 +1,18 @@
-// async function fetchCursos() {
-//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
-//   const json = await response.json();
-//   handleCursos(json);
-// }
-// fetchCursos();
-
-// function handleCursos(data: unknown) {
-//   if (data instanceof Array) {
-//     console.log('É instância de Array');
-//   }
-//   if (Array.isArray(data)) {
-//     console.log('É array');
-//   }
-// }
-
-// function isString(value: unknown): value is string {
-//   return typeof value === 'string';
-// }
-
-// function handleData(data: unknown) {
-//   if (isString(data)) {
-//     data.toUpperCase();
-//   }
-// }
-
-// handleData('Origamid');
-// handleData(200);
-
-
-
 async function fetchProduto() {
   const response = await fetch('https://api.origamid.dev/json/notebook.json');
   const json = await response.json();
-  handleProduto("json")
+  handleProduto('json');
 }
 
-fetchProduto()
+fetchProduto();
 
 interface Produto {
   nome: string;
-  preco: number
+  preco: number;
 }
 
 function isProduto(value: unknown): value is Produto {
-  if (value && typeof value === 'object' && "nome" in value) {
+  if (value && typeof value === 'object' && 'nome' in value) {
     return true;
   } else {
     return false;
@@ -53,6 +22,5 @@ function isProduto(value: unknown): value is Produto {
 function handleProduto(data: unknown) {
   if (isProduto(data)) {
     console.log(data.nome);
-
   }
 }
