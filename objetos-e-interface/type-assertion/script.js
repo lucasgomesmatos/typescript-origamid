290f40c7210af6527c4c6422e863d824e3ba6589
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-15 23:26:33 -0300

    operator non null

--- objetos-e-interface/type-assertion/script.js
+++ objetos-e-interface/type-assertion/script.js
@@ -1,37 +1,14 @@
 "use strict";
-// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
-// 2 - Defina a interface da API
-// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
-// 4 - Use Type Guards para garantir a Type Safety do código
-// 5 - Preencha os dados da API na tela.
-async function getCursos() {
-    const data = await fetch('https://api.origamid.dev/json/cursos.json');
-    const json = await data.json();
-    handleCursos(json);
+async function fetchProduto() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    return response.json();
 }
-getCursos();
-function isCurso(value) {
-    if (value &&
-        typeof value === 'object' &&
-        'nome' in value &&
-        'horas' in value &&
-        'tags' in value) {
-        return true;
-    }
-    else {
-        return false;
-    }
+async function handleProduto() {
+    const produto = await fetchProduto();
+    produto.nome;
 }
-function handleCursos(data) {
-    if (Array.isArray(data)) {
-        data.filter(isCurso).forEach((item) => {
-            document.body.innerHTML += `
-        <div>
-          <h2>${item.nome}</h2>
-          <p>${item.horas}</p>
-          <p>${item.tags.join(', ')}</p>
-        </div>
-      `;
-        });
-    }
-}
+const video = document.querySelector('video');
+video.volume;
+const video2 = document.querySelector('.player');
+const video3 = document.querySelector('.player');
+const video4 = document.querySelector('.player');
