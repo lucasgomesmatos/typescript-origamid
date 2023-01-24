8024990f1d512fa1271d3ac2ba3dfa2bfdfb57b6
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-23 22:23:25 -0300

    ex

---
+++ objetos-e-interface/intersection/script.js
@@ -0,0 +1,9 @@
+"use strict";
+function handleProdutoCarro(dados) {
+    dados.rodas;
+    dados.portas;
+    dados.preco;
+    return dados;
+}
+console.log(handleProdutoCarro({ rodas: 4, portas: 4, preco: 20000 }));
+window.userId = 200;
