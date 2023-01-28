19b4e1372ca305d9dc371550d4110c510f8f03cc
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-27 23:42:03 -0300

    class typescript

---
+++ script.ts
@@ -0,0 +1,12 @@
+class Produto {
+  tipo = 'produto';
+  nome = 'O senhor dos Anéis';
+
+  constructor(nome: string, tipo: string) {
+    this.nome = nome;
+    this.tipo = tipo;
+  }
+}
+
+const livro = new Produto('Livro', 'O senhor dos anéis');
+console.log(livro.tipo);
