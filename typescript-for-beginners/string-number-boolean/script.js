2f0660686eb077eb98d5a1a01246b71da0887e50
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-09-28 22:09:42 -0300

    adiciona a pasta `string, number e boolean`

---
+++ typescript-for-beginners/string-number-boolean/script.js
@@ -0,0 +1,9 @@
+"use strict";
+const frase = "Front-End";
+const preco = 300;
+const condicao = preco > 200;
+if (typeof frase === "string") {
+    console.log("É uma string");
+}
+console.log(typeof frase === "string");
+console.log(typeof preco);
