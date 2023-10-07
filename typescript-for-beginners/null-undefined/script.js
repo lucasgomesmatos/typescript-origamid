add155280a1625297b006f8a9e968ed64cfe3ff2
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-10-06 21:14:33 -0300

    adiona diretório

---
+++ typescript-for-beginners/null-undefined/script.js
@@ -0,0 +1,6 @@
+"use strict";
+function normalizar(texto) {
+    return texto.trim().toLowerCase();
+}
+console.log(normalizar('   Design'));
+// console.log(normalizar(200));
---
+++ typescript-for-beginners/null-undefined/script.ts
@@ -0,0 +1,6 @@
+function normalizar(texto: any) {
+  return texto.trim().toLowerCase();
+}
+
+console.log(normalizar('   Design'));
+// console.log(normalizar(200));
