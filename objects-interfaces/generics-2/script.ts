a80108d23c25ec0a450fd59490db51698ca9285e
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-11-03 22:16:53 -0300

    generics dom

--- objects-interfaces/generics-2/script.ts
+++ objects-interfaces/generics-2/script.ts
@@ -11,8 +11,14 @@ if (link) {
   console.log(extractText(link));
 }
 
-function $<T>(seletor: string): T | null {
+function $<T extends Element>(seletor: string): T | null {
   return document.querySelector(seletor);
 }
 
 const body = $<HTMLBodyElement>('body');
+
+const linkAnchor = document.querySelector('.link');
+
+if (linkAnchor instanceof HTMLAnchorElement) {
+  console.log(linkAnchor.href);
+}
