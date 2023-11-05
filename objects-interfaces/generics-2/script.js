108c99ae56abf4aff25c71e46b51f132d6895fa6
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-11-04 23:14:01 -0300

    fetch data generics

--- objects-interfaces/generics-2/script.js
+++ objects-interfaces/generics-2/script.js
@@ -1,11 +1,28 @@
 "use strict";
-function extractText(el) {
-    return {
-        texto: el.innerHTML,
-        el,
-    };
+// function extractText<T extends HTMLElement>(el: T) {
+//   return {
+//     texto: el.innerHTML,
+//     el,
+//   };
+// }
+// const link = document.querySelector('a');
+// if (link) {
+//   console.log(extractText(link));
+// }
+// function $<T extends Element>(seletor: string): T | null {
+//   return document.querySelector(seletor);
+// }
+// const body = $<HTMLBodyElement>('body');
+// const linkAnchor = document.querySelector('.link');
+// if (linkAnchor instanceof HTMLAnchorElement) {
+//   console.log(linkAnchor.href);
+// }
+async function getData(url) {
+    const response = await fetch(url);
+    return await response.json();
 }
-const link = document.querySelector('a');
-if (link) {
-    console.log(extractText(link));
+async function handleData() {
+    const notebook = await getData('https://api.origamid.dev/json/api/notebook.json');
+    console.log(notebook);
 }
+handleData();
--- objects-interfaces/generics-2/script.ts
+++ objects-interfaces/generics-2/script.ts
@@ -1,24 +1,43 @@
-function extractText<T extends HTMLElement>(el: T) {
-  return {
-    texto: el.innerHTML,
-    el,
-  };
+// function extractText<T extends HTMLElement>(el: T) {
+//   return {
+//     texto: el.innerHTML,
+//     el,
+//   };
+// }
+
+// const link = document.querySelector('a');
+
+// if (link) {
+//   console.log(extractText(link));
+// }
+
+// function $<T extends Element>(seletor: string): T | null {
+//   return document.querySelector(seletor);
+// }
+
+// const body = $<HTMLBodyElement>('body');
+
+// const linkAnchor = document.querySelector('.link');
+
+// if (linkAnchor instanceof HTMLAnchorElement) {
+//   console.log(linkAnchor.href);
+// }
+
+async function getData<T>(url: string): Promise<T> {
+  const response = await fetch(url);
+  return await response.json();
 }
 
-const link = document.querySelector('a');
-
-if (link) {
-  console.log(extractText(link));
+interface Notebook {
+  nome: string;
+  preco: number;
 }
 
-function $<T extends Element>(seletor: string): T | null {
-  return document.querySelector(seletor);
+async function handleData() {
+  const notebook = await getData<Notebook>(
+    'https://api.origamid.dev/json/api/notebook.json',
+  );
+  console.log(notebook);
 }
 
-const body = $<HTMLBodyElement>('body');
-
-const linkAnchor = document.querySelector('.link');
-
-if (linkAnchor instanceof HTMLAnchorElement) {
-  console.log(linkAnchor.href);
-}
+handleData();
