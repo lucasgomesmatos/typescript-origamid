b40d298bce38f45900b228a08211cbe35a610a5d
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-11-13 18:47:06 -0300

    adicionando dir `type guard - 2`

--- objects-interfaces/type-guard-2/script.js
+++ objects-interfaces/type-guard-2/script.js
@@ -1,36 +1,26 @@
 "use strict";
-function typeGuard(valor) {
-    if (typeof valor === 'string') {
-        return String(valor);
-    }
-    if (typeof valor === 'number') {
-        return Number(valor);
-    }
-    if (valor instanceof HTMLElement) {
-        return valor.innerHTML;
-    }
-    return valor;
-}
-// console.log(typeGuard(5.6));
-// console.log(typeGuard('5.6'));
-// console.log(typeGuard(document.querySelector('h1')));
-const obg = {
-    nome: 'João',
-};
-if ('nome' in obg) {
-    console.log(obg.nome);
-}
-async function fetchProduto() {
+async function fetchCursos() {
     const response = await fetch('https://api.origamid.dev/json/notebook.json');
     const json = await response.json();
-    handleProduto(json);
+    handleCursos(json);
 }
-function handleProduto(data) {
-    if ('preco' in data) {
-        document.body.innerHTML += `
-      <p>Nome: ${data.nome}</p>
-      <p>Preço: R$ ${data.preco + 100}</p>
-    `;
+fetchCursos();
+function isProduto(value) {
+    if (value &&
+        typeof value === 'object' &&
+        'nome' in value &&
+        'preco' in value) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
-fetchProduto();
+function handleCursos(data) {
+    if (isProduto(data)) {
+        console.log(data.nome);
+        console.log(data.preco);
+    }
+    else {
+    }
+}
--- objects-interfaces/type-guard-2/script.ts
+++ objects-interfaces/type-guard-2/script.ts
@@ -1,26 +1,33 @@
 async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
   const json = await response.json();
   handleCursos(json);
 }
 
 fetchCursos();
 
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+function isProduto(value: unknown): value is Produto {
+  if (
+    value &&
+    typeof value === 'object' &&
+    'nome' in value &&
+    'preco' in value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function handleCursos(data: unknown) {
-  if (data instanceof Array) {
-    console.log('É instância de Array');
-  }
-  if (Array.isArray(data)) {
-    console.log('É array');
-  }
-}
-
-function isString(value: unknown): value is string {
-  return typeof value === 'string';
-}
-
-function handleData(data: unknown) {
-  if (isString(data)) {
-    data.toLowerCase();
+  if (isProduto(data)) {
+    console.log(data.nome);
+    console.log(data.preco);
+  } else {
   }
 }
